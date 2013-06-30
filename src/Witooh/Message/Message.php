efa8c7309e35589f1c4d7fbbea77fc0c3e999323
witooh <witoo@outlook.com>
2013-06-30 12:53:23 +0700

    update

--- src/Witooh/Message/Message.php
+++ src/Witooh/Message/Message.php
@@ -1,17 +1,24 @@
 <?php
 
 namespace Witooh\Message;
+
 use App;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Contracts\ArrayableInterface;
 use Response;
+use Witooh\Message\Exceptions\AuthenticateException;
+use Witooh\Message\Exceptions\NotFoundException;
+use Witooh\Message\Exceptions\PermissionException;
+use Witooh\Message\Exceptions\ValidateException;
 
 class Message
 {
-    const SUCCESS    = 200;
-    const ERROR      = 500;
+    const SUCCESS = 200;
+    const ERROR = 500;
     const VALIDATION = 501;
     const PERMISSION = 400;
-    const AUTH       = 401;
-    const NOTFOUND   = 404;
+    const AUTH = 401;
+    const NOTFOUND = 404;
 
     protected $header;
 
@@ -23,111 +30,186 @@ class Message
     function __construct()
     {
         $this->header = null;
-        $this->body   = null;
-        $this->isRaw  = false;
+        $this->body = null;
+        $this->isRaw = false;
 
-        App::error(function(\Exception $exception, $code)
-        {
-            $res = Response::make($exception->getMessage(), $code);
-            $res->headers->add(array(
-                    'Content-Type'=>'application/json'
-                ));
+        App::error(
+            function (ValidateException $exception, $code) {
+                $res = Response::make($exception->getMessage(), $code);
+                $res->headers->add(
+                    array(
+                        'Content-Type' => 'application/json'
+                    )
+                );
 
-            return $res;
-        });
+                return $res;
+            }
+        );
+
+        App::error(
+            function (AuthenticateException $exception, $code) {
+                $res = Response::make($exception->getMessage(), $code);
+                $res->headers->add(
+                    array(
+                        'Content-Type' => 'application/json'
+                    )
+                );
+
+                return $res;
+            }
+        );
+
+        App::error(
+            function (NotFoundException $exception, $code) {
+                $res = Response::make($exception->getMessage(), $code);
+                $res->headers->add(
+                    array(
+                        'Content-Type' => 'application/json'
+                    )
+                );
+
+                return $res;
+            }
+        );
+
+        App::error(
+            function (PermissionException $exception, $code) {
+                $res = Response::make($exception->getMessage(), $code);
+                $res->headers->add(
+                    array(
+                        'Content-Type' => 'application/json'
+                    )
+                );
+
+                return $res;
+            }
+        );
     }
 
-    public function raw(array $content){
+    public function raw($data)
+    {
         $this->clear();
         $this->isRaw = true;
-        $this->body = $content;
-        return $this->toJson();
+        if ($data instanceof Collection) {
+            $this->body = $data->toArray();
+        } else {
+            $this->body = $data;
+        }
+
+        return $this->toArray();
     }
 
-    public function success(array $data = array(), array $header = array()){
+    public function success($data, array $header = array())
+    {
         $this->clear();
-        $this->body = $data;
+        if ($data instanceof ArrayableInterface) {
+            $this->body = $data->toArray();
+        } else {
+            $this->body = $data;
+        }
         $this->header = array(
-            'status'=>self::SUCCESS,
-            'message'=>'Success',
+            'status' => self::SUCCESS,
+            'message' => 'Success',
         );
         $this->header = array_merge($this->header, $header);
-        return $this->toJson();
+
+        return $this->toArray();
     }
 
-    public function permission($message = 'Permission Denied', $header = array()){
+    public function permission($message = 'Permission Denied', $header = array())
+    {
         $this->clear();
         $this->header = array(
-            'status'=>self::PERMISSION,
-        );
-        $this->header = array_merge($this->header, $header);
-        $this->header['message'] = $message;
-        return $this->toJson();
-    }
-
-    public function error($message, array $data = array(), array $header = array()){
-        $this->clear();
-        $this->body = $data;
-        $this->header = array(
-            'status'=>self::ERROR,
-            'message'=>'Error',
-        );
-        $this->header = array_merge($this->header, $header);
-        $this->header['message'] = $message;
-        return $this->toJson();
-    }
-
-    public function validation(array $data, array $header = array()){
-        $this->clear();
-        $this->body = $data;
-        $this->header = array(
-            'status'=>self::VALIDATION,
-            'message'=>'Validation Failed',
-        );
-        $this->header = array_merge($this->header, $header);
-        return $this->toJson();
-    }
-
-    public function auth($message = 'Authenticate is required', array $header = array()){
-        $this->clear();
-        $this->header = array(
-            'status'=>self::AUTH,
+            'status' => self::PERMISSION,
         );
         $this->header = array_merge($this->header, $header);
         $this->header['message'] = $message;
-        return $this->toJson();
+
+        return $this->toArray();
     }
 
-    public function notfound($message = 'The request not found', $header = array()){
+    public function error($message, array $data = array(), array $header = array())
+    {
+        $this->clear();
+        $this->body = $data;
+        $this->header = array(
+            'status' => self::ERROR,
+            'message' => 'Error',
+        );
+        $this->header = array_merge($this->header, $header);
+        $this->header['message'] = $message;
+
+        return $this->toArray();
+    }
+
+    public function validation($data = array(), array $header = array())
+    {
+        $this->clear();
+        if ($data instanceof ArrayableInterface) {
+            $this->body = $data->toArray();
+        } else {
+            $this->body = $data;
+        }
+        $this->header = array(
+            'status' => self::VALIDATION,
+            'message' => 'Validation Failed',
+        );
+        $this->header = array_merge($this->header, $header);
+
+        return $this->toArray();
+    }
+
+    public function auth($message = 'Authenticate is required', array $header = array())
+    {
+        $this->clear();
+        $this->header = array(
+            'status' => self::AUTH,
+        );
+        $this->header = array_merge($this->header, $header);
+        $this->header['message'] = $message;
+
+        return $this->toArray();
+    }
+
+    public function notfound($message = 'The request not found', $header = array())
+    {
         $this->clear();
         $this->body = null;
         $this->header = array(
-            'status'=>self::NOTFOUND,
+            'status' => self::NOTFOUND,
         );
         $this->header = array_merge($this->header, $header);
         $this->header['message'] = $message;
+
         return $this->toJson();
     }
 
-    public function clear(){
+    public function clear()
+    {
         $this->body = null;
         $this->header = null;
         $this->isRaw = false;
     }
 
-    public function toJson(){
-        if($this->isRaw){
+    public function toArray()
+    {
+        if ($this->isRaw) {
             $arr = $this->body;
-        }else{
+        } else {
             $arr = array();
-            if(!empty($this->header)){
+            if (!empty($this->header)) {
                 $arr['header'] = $this->header;
             }
-            if(!empty($this->body)){
+            if (!empty($this->body)) {
                 $arr['body'] = $this->body;
             }
         }
 
-        return json_encode($arr);
+        return $arr;
+    }
+
+    public function toJson($options = 0)
+    {
+        return json_encode($this->toArray(), $options);
     }
 }
